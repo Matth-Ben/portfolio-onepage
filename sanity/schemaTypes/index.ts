d8efe753fc92512e72767850057f6c8951f91d4a
Matth-Ben <matthiasbenoit.pro@gmail.com>
2024-02-19 23:38:25 +0100

    :tada: [INIT] Start sanity

---
+++ sanity/schemaTypes/index.ts
@@ -0,0 +1,6 @@
+import navigations from './documents/navigation'
+import navItem from './objects/navItem'
+import link from './objects/link'
+import page from './pageTypes'
+
+export const schemaTypes = [navigations, navItem, link, page]
