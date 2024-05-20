cae2700a55580ed2e61078a6bed5896db370d2b0
Matth-Ben <matthiasbenoit.pro@gmail.com>
2024-05-20 23:25:37 +0200

    :sparkles: [CARD-PROJECT] Test

--- app/components/CardProject.jsx
+++ app/components/CardProject.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
+'use client'
+
+import React, { useState, useEffect } from 'react'
 import Image from 'next/image';
-import { urlFor } from '../lib/sanity';
 import Link from 'next/link'
+import { urlFor } from '../lib/sanity';
+import variables from '../variables.module.scss'
 
 const CardProject = ({data}) => {
+    const [classActive, classText] = useState('')
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+        classText(`${variables.visible}`)
+        }, 300)
+    }, [])
+
     return (
-        <Link href={`/projects/${data.slug.current}`} className="border-2 border-gray-500 rounded-lg p-1 hover:scale-105 hover:border-blue-500 transition">
-            <Image src={urlFor(data.image).url()} width={400} height={200} alt={data.image.alt} />
-            <h4>{data.title}</h4>
+        <Link href={`/projects/${data.slug.current}`} className="">
+            <section className={`${variables.title} h-screen`}>
+                <div className={`${variables.popin} ${classActive}`}>
+                    <div className={`${variables.popin_animA}`}>
+                        <div className={`${variables.popin_animB}`}>
+                            <Image src={urlFor(data.image).url()} width={400} height={200} alt={data.image.alt} />
+
+                            <h1 className='title'>{data.title}</h1>
+                        </div>
+                    </div>  
+                </div>
+            </section>
         </Link>
     )
 }
